19307fe8ed4094aba890786d381867dd8dd75c63
Evgeny Volovlikov <volovlikovevgeny@mail.ru>
2020-10-06 14:33:28 +0300

    adding homepage component directory component and styling them

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import './directory.styles.scss';
+
+const Directory = () => (
+    <div className='directory-menu'>
+        <div className='menu-item'>
+            <div className='content'>
+                <h1 className='title'>Title</h1>
+                <span className='subtitle'>Shop Now</span>
+            </div>
+        </div>
+        <div className='menu-item'>
+            <div className='content'>
+                <h1 className='title'>Title</h1>
+                <span className='subtitle'>Shop Now</span>
+            </div>
+        </div>
+        <div className='menu-item'>
+            <div className='content'>
+                <h1 className='title'>Title</h1>
+                <span className='subtitle'>Shop Now</span>
+            </div>
+        </div>
+        <div className='menu-item'>
+            <div className='content'>
+                <h1 className='title'>Title</h1>
+                <span className='subtitle'>Shop Now</span>
+            </div>
+        </div>
+        <div className='menu-item'>
+            <div className='content'>
+                <h1 className='title'>Title</h1>
+                <span className='subtitle'>Shop Now</span>
+            </div>
+        </div>
+    </div>
+)
+
+export default Directory
