dcccb454e1f1a13aee4c6105be83363373396a22
Evgeny Volovlikov <volovlikovevgeny@mail.ru>
2020-10-06 16:07:39 +0300

    adding react-router-dom && update menu-items component withRouter HOC

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import Homepage from './pages/homepage/homepage.component';
+import { Switch, Route } from 'react-router-dom'
 
 import './App.css';
 
 function App() {
+
+  const HatsPage = (props) => {
+
+    return (
+      <div>
+        <h1>HATS PAGE</h1>
+      </div>
+    )
+  }
   return (
-    <div className="App">
-      <Homepage />
+    <div>
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
+
     </div>
   );
 }
