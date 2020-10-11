60e230be87e7c9d2f750ca0bdb51df1ed8222610
Evgeny Volovlikov <volovlikovevgeny@mail.ru>
2020-10-11 18:49:41 +0300

    adding sig-up comp refresh with authentication and registration and log in

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import { Switch, Route } from 'react-router-dom'
 
 import './App.css';
 
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
 
@@ -24,25 +24,35 @@ class App extends React.Component {
 
   unsubscribeFromAuth = null;
 
-
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({
-        currentUser: user
-      })
-    })
-  }
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
 
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+
+          console.log(this.state);
+        });
+      }
+
+      this.setState({ currentUser: userAuth });
+    });
+  }
 
   componentWillUnmount() {
     this.unsubscribeFromAuth();
   }
 
   render() {
-    console.log(this.state);
     return (
       <div>
-        <Header />
+        <Header currentUser={this.state.currentUser} />
         <Switch>
           <Route exact path='/' component={Homepage} />
           <Route path='/shoppage' component={Shoppage} />
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,20 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { ReactComponent as Logo } from '../../assets/crown.svg'
+import { auth } from '../../firebase/firebase.utils';
 
 import './header.styles.scss'
 
-const Header = () => (
-    <div className='header'>
-        <Link className='logo-container' to='/' >
-            <Logo className='logo' />
-        </Link>
-        <div className='options'>
-            <Link className='option' to='/shoppage'>SHOP</Link>
-            <Link className='option' to='/shoppage'>CONTACT</Link>
-            <Link className='option' to='/signin'>SIGN IN</Link>
+const Header = ({ currentUser }) => {
+    return (
+        <div className='header'>
+            <Link className='logo-container' to='/' >
+                <Logo className='logo' />
+            </Link>
+            <div className='options'>
+                <Link className='option' to='/shoppage'>SHOP</Link>
+                <Link className='option' to='/shoppage'>CONTACT</Link>
+                {currentUser ? (
+                    <div className='option' onClick={() => auth.signOut()}>
+                        SIGN OUT
+                    </div>
+                ) : (
+                        <Link className='option' to='/signin'>SIGN IN</Link>
+                    )}
+            </div>
         </div>
-    </div>
-)
+    )
+}
+
 
 export default Header
